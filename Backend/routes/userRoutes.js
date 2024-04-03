1a1d9bde1984bbdf2bf3c355bec298d7216eccdf
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-03 14:59:42 +0530

    userRoutes done

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const { UserModel } = require("../model/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userRoute = express.Router();
 
 userRoute.post("/register", async (req, res) => {
-  const payload = req.body;
+  const { name, email, pass } = req.body;
   try {
-    const user = new UserModel(payload);
-    await user.save();
-    res.send({ msg: "new User has been Registered Successful" });
+    bcrypt.hash(pass, 5, async (err, hash) => {
+      // Store hash in your password DB.
+      if (err) {
+        res.send({ msg: "Something Went Wrong", error: err.message });
+      } else {
+        const user = new UserModel({ name, email, pass: hash });
+        await user.save();
+        res.send({ msg: "new User has been Registered Successful" });
+      }
+    });
   } catch (error) {
     res.send({ msg: "Something Went Wrong", error: error.message });
   }
@@ -17,11 +25,17 @@ userRoute.post("/register", async (req, res) => {
 
 userRoute.post("/login", async (req, res) => {
   const { email, pass } = req.body;
-  const token = jwt.sign({ course: "bacakend" }, "masai");
   try {
-    const user = await UserModel.find({ email, pass });
+    const user = await UserModel.find({ email });
     if (user.length > 0) {
-      res.send({ msg: "logged in Successful", token: token });
+      bcrypt.compare(pass, user[0].pass, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ course: "bacakend" }, "masai");
+          res.send({ msg: "logged in Successful", token: token });
+        } else {
+          res.send({ msg: "Wrong Credential" });
+        }
+      });
     } else {
       res.send({ msg: "Wrong Credential" });
     }
