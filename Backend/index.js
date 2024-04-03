4001da8af2e3da1c75f3c8bc63c3bc14b7852788
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-03 13:59:48 +0530

    setup my backend project

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { Connection } = require("./config/db");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome Shaktiman");
+});
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await Connection;
+    console.log("Connected to Mongo-DB");
+  } catch (error) {
+    console.log("Did Not Connected to Mongo-DB");
+    Console.log({ msg: error.message });
+  }
+  console.log(`Server ise Running at Port ${process.env.PORT}`);
+});
